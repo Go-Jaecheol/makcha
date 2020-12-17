d6c91bf1d816d2c442c31fd20223a66a8c132f31
thisisyoobi <thisisyoobi@gmail.com>
2020-12-18 05:26:19 +0900

    define Class

--- app_code/Makcha/app/src/main/java/com/example/makcha/GetLastBusInfo.java
+++ app_code/Makcha/app/src/main/java/com/example/makcha/GetLastBusInfo.java
@@ -1,15 +1,32 @@
 package com.example.makcha;
 
 public class GetLastBusInfo {
-    protected String getLastBusNumber(){
-        return "937";
+    private String lastBusNumer = "";
+    private int lastBusRemainTime = 0;
+
+    protected void getLastBusInfo(String starting_p, String finish_p){
+        // 1. 서버에게 출발지와 목적지 전달
+        // 2. 막차 버스 정보를 받는다
+
+        // 3-1. 받은 정보 중 버스 번호 저장
+        lastBusNumer = "937";
+        // 3-2. 받은 정보 막차까지 남은 예상 분 저장
+        lastBusRemainTime = 90;
     }
 
-    protected String setLastBusArrivalTime() {
+    protected String getLastBusAnnounceMsg() {
+        int hour = lastBusRemainTime / 60;
+        int min = lastBusRemainTime % 60;
+        String msg = "막차 " + String.valueOf(hour) + "시간 " + String.valueOf(min) + "분 후 도착 예정";
 
-        return "막차 1시간 30분 후 도착 예정";
+        return msg;
     }
-    protected int setLastBussArrivalTime() {
-        return 2;
+
+    public String getLastBusNumer() {
+        return lastBusNumer;
+    }
+
+    public int getLastBusRemainTime() {
+        return lastBusRemainTime;
     }
 }
